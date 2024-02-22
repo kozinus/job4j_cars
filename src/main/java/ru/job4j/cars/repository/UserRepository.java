f67f3bdb015983a56a8da163f52b9c4d5be2a75a
kozinus <kkozin2403@gmail.com>
2024-02-22 16:06:30 +0300

    2. HQL [#6874 #482080]

--- src/main/java/ru/job4j/cars/repository/UserRepository.java
+++ src/main/java/ru/job4j/cars/repository/UserRepository.java
@@ -6,6 +6,7 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import ru.job4j.cars.model.User;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -21,10 +22,14 @@ public class UserRepository {
      */
     public User create(User user) {
         Session session = sf.openSession();
-        session.beginTransaction();
-        session.save(user);
-        session.getTransaction().commit();
-        session.close();
+        try {
+            session.beginTransaction();
+            session.save(user);
+            session.getTransaction().commit();
+            session.close();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        }
         return user;
     }
 
@@ -34,10 +39,14 @@ public class UserRepository {
      */
     public void update(User user) {
         Session session = sf.openSession();
-        session.beginTransaction();
-        session.update(user);
-        session.getTransaction().commit();
-        session.close();
+        try {
+            session.beginTransaction();
+            session.update(user);
+            session.getTransaction().commit();
+            session.close();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        }
     }
 
     /**
@@ -46,12 +55,17 @@ public class UserRepository {
      */
     public void delete(int userId) {
         Session session = sf.openSession();
-        session.beginTransaction();
-        User user = new User();
-        user.setId(userId);
-        session.delete(user);
-        session.getTransaction().commit();
-        session.close();
+        try {
+            session.beginTransaction();
+            User user = new User();
+            user.setId(userId);
+            session.createQuery("delete ru.job4j.cars.model.User where id = :Id", User.class)
+                    .setParameter("Id", userId).uniqueResult();
+            session.getTransaction().commit();
+            session.close();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        }
     }
 
     /**
@@ -60,11 +74,16 @@ public class UserRepository {
      */
     public List<User> findAllOrderById() {
         Session session = sf.openSession();
-        session.beginTransaction();
-        List<User> result = session.createQuery(
-                "from ru.job4j.cars.model.User as p order by p.id asc", User.class).list();
-        session.getTransaction().commit();
-        session.close();
+        List<User> result = new ArrayList<>();
+        try {
+            session.beginTransaction();
+            result = session.createQuery(
+                    "from ru.job4j.cars.model.User as p order by p.id asc", User.class).list();
+            session.getTransaction().commit();
+            session.close();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        }
         return result;
     }
 
@@ -74,10 +93,18 @@ public class UserRepository {
      */
     public Optional<User> findById(int userId) {
         Session session = sf.openSession();
-        session.beginTransaction();
-        Optional<User> result = Optional.of(session.get(User.class, userId));
-        session.getTransaction().commit();
-        session.close();
+        Optional<User> result = Optional.empty();
+        try {
+            session.beginTransaction();
+            result = Optional.of(session.createQuery(
+                            "from ru.job4j.cars.model.User as p where p.id = :Id", User.class)
+                    .setParameter("Id", userId)
+                    .getSingleResult());
+            session.getTransaction().commit();
+            session.close();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        }
         return result;
     }
 
@@ -88,13 +115,18 @@ public class UserRepository {
      */
     public List<User> findByLikeLogin(String key) {
         Session session = sf.openSession();
-        session.beginTransaction();
-        List<User> result = session.createQuery(
-                "from ru.job4j.cars.model.User as p where p.login like ?1", User.class)
-                .setParameter(1, "%" + key + "%")
-                .list();
-        session.getTransaction().commit();
-        session.close();
+        List<User> result = new ArrayList<>();
+        try {
+            session.beginTransaction();
+            result = session.createQuery(
+                            "from ru.job4j.cars.model.User as p where p.login like ?1", User.class)
+                    .setParameter(1, "%" + key + "%")
+                    .list();
+            session.getTransaction().commit();
+            session.close();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        }
         return result;
     }
 
@@ -105,13 +137,18 @@ public class UserRepository {
      */
     public Optional<User> findByLogin(String login) {
         Session session = sf.openSession();
-        session.beginTransaction();
-        Optional<User> result = session.createQuery(
-                        "from ru.job4j.cars.model.User as p where p.login = :login", User.class)
-                .setParameter("login", login)
-                .uniqueResultOptional();
-        session.getTransaction().commit();
-        session.close();
+        Optional<User> result = Optional.empty();
+        try {
+            session.beginTransaction();
+            result = session.createQuery(
+                            "from ru.job4j.cars.model.User as p where p.login = :login", User.class)
+                    .setParameter("login", login)
+                    .uniqueResultOptional();
+            session.getTransaction().commit();
+            session.close();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        }
         return result;
     }
 }
